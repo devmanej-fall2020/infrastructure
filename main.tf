34f09484509f36a8caee1a229c08157e786fb1e5
jaidevmane <devmane.j@northeastern.edu>
2020-10-22 07:48:01 -0400

    adding rds and ec2 to infra

--- main.tf
+++ main.tf
@@ -83,3 +83,248 @@ resource "aws_route_table_association" "rt_association_3" {
   subnet_id      = aws_subnet.subnet3.id
   route_table_id = aws_route_table.r_table.id
 }
+
+
+resource "aws_security_group" "application_security_group" {
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.assignmentvpc.id
+
+  ingress {
+    description = "Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block_map["cidr_route"]]
+  }
+
+  ingress {
+    description = "Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block_map["cidr_route"]]
+  }
+
+  ingress {
+    description = "Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block_map["cidr_route"]]
+  }
+
+  ingress {
+    description = "Port 4000"
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = [var.cidr_block_map["cidr_route"]]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "application_security_group"
+  }
+}
+
+
+resource "aws_security_group" "database_security_group"{
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.assignmentvpc.id
+
+
+  ingress {
+    description = "Allow MySQL access"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.application_security_group.id]
+}
+
+
+  tags = {
+    Name = "database_security_group"
+  }
+}
+
+
+resource "aws_s3_bucket" "assignmentbucket" {
+  bucket = "webapp.jaisubash.devmane"
+  acl    = "private"
+  force_destroy = true
+
+
+  server_side_encryption_configuration {    
+    rule {     
+       apply_server_side_encryption_by_default { sse_algorithm = "AES256"}
+       }
+  }
+
+  lifecycle_rule {
+    id      = "log"
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
+    }
+  }
+
+  tags = {
+    Name = "assignmentbucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3removePublicAccess" {
+bucket = aws_s3_bucket.assignmentbucket.id
+block_public_acls = true
+block_public_policy = true
+restrict_public_buckets = true
+ignore_public_acls = true
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db_subnet_group"
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id,aws_subnet.subnet3.id]
+
+  tags = {
+    Name = "DB Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "rdsassignmentdb" {
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "mysql"
+  instance_class = "db.t2.micro"
+  name = var.cred_vars["name"]
+  username = var.cred_vars["username"]
+  password = var.cred_vars["password"]
+  multi_az = false
+  publicly_accessible = false
+  identifier = var.cred_vars["identifier"]
+  skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.database_security_group.id]
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+}
+
+data "aws_ami" "amigetfunction" {
+  owners = ["411821511450"]
+  most_recent = true
+
+}
+
+
+resource "aws_instance" "ec2instance-assignment" {
+  ami = data.aws_ami.amigetfunction.id
+  instance_type = "t2.micro"
+  key_name = var.cred_vars["key_name"]
+  vpc_security_group_ids = [aws_security_group.application_security_group.id]
+  subnet_id = aws_subnet.subnet1.id
+  iam_instance_profile = aws_iam_instance_profile.ec2_iam_ip.name
+  associate_public_ip_address = true
+  disable_api_termination = false
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo touch .env\n
+                sudo echo "export RDS_DB_USERNAME=${var.cred_vars["username"]}" >> /home/ubuntu/.bashrc
+                sudo echo "export RDS_DB_PASSWORD=${var.cred_vars["password"]}" >> /home/ubuntu/.bashrc
+                sudo echo "export RDS_DB_HOSTNAME=${aws_db_instance.rdsassignmentdb.address}" >> /home/ubuntu/.bashrc
+                sudo echo "export S3_BUCKET_NAME=${aws_s3_bucket.assignmentbucket.bucket}" >> /home/ubuntu/.bashrc
+                sudo echo "export RDS_DB_ENDPOINT=${aws_db_instance.rdsassignmentdb.endpoint}" >> /home/ubuntu/.bashrc
+                sudo echo "export RDS_DB_NAME=${aws_db_instance.rdsassignmentdb.name}" >> /home/ubuntu/.bashrc
+  EOF
+
+
+  root_block_device {
+      volume_type = "gp2"
+      volume_size =  20
+      delete_on_termination = true
+  }
+  tags = {
+    Name = "ec2instance-assignment"
+  }
+}
+
+# IAM Policy
+resource "aws_iam_policy" "wa_s3_policy" {
+  name        = "WebAppS3"
+  path        = "/"
+  description = "WebAppS3 policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::webapp.jaisubash.devmane",
+                "arn:aws:s3:::webapp.jaisubash.devmane/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_instance_profile" "ec2_iam_ip" {
+  name = "test_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+
+
+resource "aws_iam_policy_attachment" "attachement" {
+  name       = "Policy Attachment"
+  roles      = [aws_iam_role.ec2_role.name]
+  policy_arn = aws_iam_policy.wa_s3_policy.arn
+}
+
+
+
+resource "aws_dynamodb_table" "assignment-dynamodb" {
+  name           = "csye6225"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "assignment-dynamodb"
+  }
+}
